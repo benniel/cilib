b8fd7277f4d6fc0f133263aee1f642b4b057e2af
Bennie Leonard <bennie.leonard@gmail.com>
2014-05-20 17:17:10 +0200

    Incomplete Charged PSO

--- core/src/main/scala/cilib/PSO.scala
+++ core/src/main/scala/cilib/PSO.scala
@@ -40,7 +40,18 @@ object StepSize {
   }
 }
 
+trait Charge[F[_], S, A] {
+  def charge: Lens[S, Double]
+}
+
+object Charge {
+  implicit def memCharge[F[_], A] = new Charge[F, ChargedMem[F, A], A] {
+    def charge: Lens[ChargedMem[F, A], Double] = Lens.lensu((a, b) => a.copy(c = b), _.c)
+  }
+}
+
 case class Mem[F[_], A](b: Position[F, A], v: Position[F, A])
+case class ChargedMem[F[_], A](m: Mem[F, A], c: Double)
 
 object PSO {
 
@@ -50,6 +61,17 @@ object PSO {
       b <- (M.mem.get(x._1) - x._2).traverse(q => Dist.stdUniform.map(r => n.fromDouble(r * c1 * n.toDouble(q))))
       a <- (globalG.neighbour(coll, x._2) - x._2).traverse(q => Dist.stdUniform.map(r => n.fromDouble(r * c2 * n.toDouble(q))))
     } yield n.fromDouble(w) *: x._2 + b + a
+
+    newVel.map(y => (V.step.set(x._1, y), x._2))
+  }
+
+  def chargedVelocity[S, F[_]: Zip: Traverse, A: Numeric](globalG: Neighbour[F, A], M: Memory[F, S, A], V: StepSize[F, S, A], C: Charge[F, S, A])(w: Double, c1: Double, c2: Double)(coll: List[Position[F, A]], x: (S, Position[F, A])): RVar[(S, Position[F, A])] = {
+    val n = implicitly[Numeric[A]]
+    val newVel: RVar[Position[F, A]] = for {
+      b <- (M.mem.get(x._1) - x._2).traverse(q => Dist.stdUniform.map(r => n.fromDouble(r * c1 * n.toDouble(q))))
+      a <- (globalG.neighbour(coll, x._2) - x._2).traverse(q => Dist.stdUniform.map(r => n.fromDouble(r * c2 * n.toDouble(q))))
+      c <- coll.map(_.pos).reduceLeft((p1, p2) => (p1 + p2).pos) //calculate repulsion term
+    } yield n.fromDouble(w) *: x._2 + b + a + c
 
     newVel.map(y => (V.step.set(x._1, y), x._2))
   }
